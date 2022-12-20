b5f6334f65844d86a5d5830fe403336dec9f6df9
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2022-12-20 11:26:25 +0100

    network

--- index.py
+++ index.py
@@ -5,6 +5,9 @@ This app describes the Change Propagation Method (CPM) and it lets you play with
 import streamlit as st
 import pandas as pd
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+from pyvis import network as net
 
 from helpers.app_helpers import ppm, combined_likelihood_matrix, combined_risk_matrix, combined_impact_matrix, plot_product_risk_matrix
 
@@ -23,42 +26,31 @@ def make_grid(cols,rows):
 
 st.title('Change Propagation')
 
-st.header('Definitions')
-st.markdown('''
-$a$: destination of change
+with st.expander('Definitions'):
+    st.markdown('''
+    $a$: destination of change
 
-$b$: source of change
+    $b$: source of change
 
-$u$: pen-ultimate sub-system i the chain from **a** to **b**
+    $u$: pen-ultimate sub-system i the chain from **a** to **b**
 
-$\sigma_{u,a}$: Likelihood of change reaching sub-system **u** from **a**
+    $\sigma_{u,a}$: Likelihood of change reaching sub-system **u** from **a**
 
-$l_{b,u}$: Direct likelihood of change propagation from  **u** to **b**
+    $l_{b,u}$: Direct likelihood of change propagation from  **u** to **b**
 
-Risk of change propagating from **u** to **b**:
+    Risk of change propagating from **u** to **b**:
 
-$\rho_{b,u} = \sigma_{u,a} l_{b,u} i_{b,u}$
+    $\rho_{b,u} = \sigma_{u,a} l_{b,u} i_{b,u}$
 
-Combined risk of change propagating from **b** to **a**:
+    Combined risk of change propagating from **b** to **a**:
 
-$R_{b,a} = 1- \prod{(1 - \rho_{b,u})}$
-''')
+    $R_{b,a} = 1- \prod{(1 - \rho_{b,u})}$
+    ''')
 
 st.header('1. Initial Analysis')
 st.subheader('1.1. Create Product Model')
-st.markdown('''
-    a. power supply
 
-    b. motor
 
-    c. heating unit
-
-    d. fan
-
-    e. control system
-
-    f. casing
-''')
 product_components =   ['power supply',
                         'motor',
                         'heating unit',
@@ -66,10 +58,6 @@ product_components =   ['power supply',
                         'control system',
                         'casing']
 
-st.subheader('1.2. Complete Dependency Matrices')
-st.markdown('''
-Within the DSM the column headings show instigating sub-systems and the row headings the affected sub-systems, whose designs change as a result of change to the instigating sub-systems.
-''')
 # Design structure Matrix (DSM)
 DSM =  [[0,1,1,0,0,0],
         [1,0,0,1,1,1],
@@ -77,12 +65,46 @@ DSM =  [[0,1,1,0,0,0],
         [1,1,1,0,0,1],
         [0,0,1,1,0,1],
         [1,1,1,1,1,0]]
-st.write("DSM = ")
-mygrid_DSM = make_grid(len(DSM),len(DSM[0]))
-for i,row in enumerate(DSM):
-    for j,cell in enumerate(row):
-        mygrid_DSM[i][j].write(cell)
 
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown('''
+        a. power supply \n
+        b. motor \n
+        c. heating unit \n
+        d. fan \n
+        e. control system \n
+        f. casing
+    ''')
+
+with col2:
+    A = pd.DataFrame(DSM, index=product_components, columns=product_components)
+    G = nx.from_pandas_adjacency(A, create_using=nx.DiGraph)
+    # fig, ax = plt.subplots()
+    # pos = nx.kamada_kawai_layout(G)
+    # nx.draw(G,pos, with_labels=True)
+    # st.pyplot(fig)
+
+    g=net.Network(height='400px', width='400px',directed=True)
+    g.from_nx(G)
+    g.save_graph('example.html')
+    HtmlFile = open("example.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read() 
+    st.components.v1.html(source_code, height = 400,width=400)
+
+
+with st.expander("DSM = "):
+    mygrid_DSM = make_grid(len(DSM),len(DSM[0]))
+    for i,row in enumerate(DSM):
+        for j,cell in enumerate(row):
+            mygrid_DSM[i][j].write(cell)
+
+st.subheader('1.2. Complete Dependency Matrices')
+st.markdown('''
+Within the DSM the column headings show instigating sub-systems and the row headings the affected sub-systems, whose designs change as a result of change to the instigating sub-systems.
+''')
 
 # Direct likelihood matrix (l)
 direct_likelihood_matrix = [[0.0,0.3,0.3,0.0,0.0,0.0],
@@ -91,11 +113,11 @@ direct_likelihood_matrix = [[0.0,0.3,0.3,0.0,0.0,0.0],
                             [0.3,0.6,0.9,0.0,0.0,0.9],
                             [0.0,0.0,0.3,0.6,0.0,0.3],
                             [0.3,0.9,0.6,0.9,0.6,0.0]]
-st.write("Direct likelihood matrix (l) = ")
-mygrid_direct_likelihood_matrix = make_grid(len(direct_likelihood_matrix),len(direct_likelihood_matrix[0]))
-for i,row in enumerate(direct_likelihood_matrix):
-    for j,cell in enumerate(row):
-        mygrid_direct_likelihood_matrix[i][j].write(cell)
+with st.expander("Direct likelihood matrix (l) = "):
+    mygrid_direct_likelihood_matrix = make_grid(len(direct_likelihood_matrix),len(direct_likelihood_matrix[0]))
+    for i,row in enumerate(direct_likelihood_matrix):
+        for j,cell in enumerate(row):
+            mygrid_direct_likelihood_matrix[i][j].write(cell)
 
 # Direct impact matrix (i)
 direct_impact_matrix = [[0.0,0.9,0.9,0.0,0.0,0.0],
@@ -104,20 +126,22 @@ direct_impact_matrix = [[0.0,0.9,0.9,0.0,0.0,0.0],
                         [0.3,0.3,0.6,0.0,0.0,0.3],
                         [0.0,0.0,0.3,0.3,0.0,0.3],
                         [0.3,0.6,0.6,0.9,0.6,0.0]]
-st.write("Direct impact matrix (i) = ")
-mygrid_direct_impact_matrix = make_grid(len(direct_impact_matrix),len(direct_impact_matrix[0]))
-for i,row in enumerate(direct_impact_matrix):
-    for j,cell in enumerate(row):
-        mygrid_direct_impact_matrix[i][j].write(cell)
+with st.expander("Direct impact matrix (i) = "):
+    mygrid_direct_impact_matrix = make_grid(len(direct_impact_matrix),len(direct_impact_matrix[0]))
+    for i,row in enumerate(direct_impact_matrix):
+        for j,cell in enumerate(row):
+            mygrid_direct_impact_matrix[i][j].write(cell)
+
+st.write("Calculate the direct risk matrix: r = l * i")
 
 # Direct risk matrix (r)
 #TODO make into function
 direct_risk_matrix = (np.array(direct_likelihood_matrix)*np.array(direct_impact_matrix)).tolist()
-st.write("Direct risk matrix (r) = ")
-mygrid_direct_risk_matrix = make_grid(len(direct_risk_matrix),len(direct_risk_matrix[0]))
-for i,row in enumerate(direct_risk_matrix):
-    for j,cell in enumerate(row):
-        mygrid_direct_risk_matrix[i][j].write(cell)
+with st.expander("Direct risk matrix (r) = "):
+    mygrid_direct_risk_matrix = make_grid(len(direct_risk_matrix),len(direct_risk_matrix[0]))
+    for i,row in enumerate(direct_risk_matrix):
+        for j,cell in enumerate(row):
+            mygrid_direct_risk_matrix[i][j].write(cell)
 
 
 st.subheader('1.3. Compute Predictive Matrices')
@@ -139,26 +163,26 @@ $l_{b,u} \cap l_{b,v} = l_{b,u} + l_{b,v} - (l_{b,u} \times l_{b,v}) = 1 - ((1 -
 
 # Combined likelihood matrix (L)
 clm = combined_likelihood_matrix(DSM,direct_likelihood_matrix)
-st.write("Combined likelihood matrix (L) = ")
-mygrid_clm = make_grid(len(clm),len(clm[0]))
-for i,row in enumerate(clm):
-    for j,cell in enumerate(row):
-        mygrid_clm[i][j].write(cell)
+with st.expander("Combined likelihood matrix (L) = "):
+    mygrid_clm = make_grid(len(clm),len(clm[0]))
+    for i,row in enumerate(clm):
+        for j,cell in enumerate(row):
+            mygrid_clm[i][j].write(cell)
 
 # Combined risk matrix (R)
 crm = combined_risk_matrix(DSM,direct_likelihood_matrix,direct_impact_matrix)
-st.write("Combined risk matrix (R) = ")
-mygrid_crm = make_grid(len(crm),len(crm[0]))
-for i,row in enumerate(crm):
-    for j,cell in enumerate(row):
-        mygrid_crm[i][j].write(cell)
+with st.expander("Combined risk matrix (R) = "):
+    mygrid_crm = make_grid(len(crm),len(crm[0]))
+    for i,row in enumerate(crm):
+        for j,cell in enumerate(row):
+            mygrid_crm[i][j].write(cell)
 
 # Combined impact matrix (I) combined_impact_matrix
 cim = combined_impact_matrix(DSM,clm,crm)
-st.write("Combined impact matrix (I) = ")
-mygrid_cim = make_grid(len(cim),len(cim[0]))
-for i,row in enumerate(cim):
-    for j,cell in enumerate(row):
-        mygrid_cim[i][j].write(cell)
+with st.expander("Combined impact matrix (I) = "):
+    mygrid_cim = make_grid(len(cim),len(cim[0]))
+    for i,row in enumerate(cim):
+        for j,cell in enumerate(row):
+            mygrid_cim[i][j].write(cell)
 
 plot_product_risk_matrix(product_components,DSM,clm,cim,crm)
