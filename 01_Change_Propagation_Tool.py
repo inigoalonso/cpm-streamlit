aa9d5750b4f942cae8e62253cfa3ce2a74efb9fc
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2023-02-07 22:35:14 +0100

    plots

--- 01_Change_Propagation_Tool.py
+++ 01_Change_Propagation_Tool.py
@@ -9,6 +9,7 @@ import csv
 import io
 import zipfile
 import time
+import plotly.express as px
 
 from cpm.cpm import calculate_all_matrices
 
@@ -24,26 +25,30 @@ except:
 
 st.title('Change Propagation Tool')
 
-# Upload files
-st.header('1. Input files')
-uploaded_files = st.file_uploader(
-    "Files, e.g. dsm01.csv, dsm02.csv, ...",
-    type="csv",
-    accept_multiple_files=True
-)
+in_col1, in_col2 = st.columns(2)
+
+with in_col1:
+    # Upload files
+    st.header('1. Input files')
+    uploaded_files = st.file_uploader(
+        "Files, e.g. dsm01.csv, dsm02.csv, ...",
+        type="csv",
+        accept_multiple_files=True
+    )
 
 if ('uploaded_files' in locals()) and (uploaded_files != []):
-    st.header('2. Configuration')
-    with st.form(key='configuration_form'):
-        change_path_length = st.slider(
-            'Change path length',
-            min_value=1,
-            max_value=10,
-            value=3,
-            step=1,
-            help='The maximum number of changes that can be propagated through the product structure.'
-            )
-        submit_button = st.form_submit_button(label='Apply CPM')
+    with in_col2:
+        st.header('2. Configuration')
+        with st.form(key='configuration_form'):
+            change_path_length = st.slider(
+                'Change path length',
+                min_value=1,
+                max_value=10,
+                value=3,
+                step=1,
+                help='The maximum number of changes that can be propagated through the product structure.'
+                )
+            submit_button = st.form_submit_button(label='Apply CPM')
     if submit_button:
         st.header('3. Results')
         result_files = {}
@@ -69,28 +74,54 @@ if ('uploaded_files' in locals()) and (uploaded_files != []):
             col_count = len(header)
             #st.write(f"Total number of columns in the present file is {col_count}")
 
-            if header[0] == 'DSM EXPORTED FROM CAMBRIDGE ADVANCED MODELLER':
-                rows_to_skip = 4
-            else:
-                rows_to_skip = 1
-
-            # Skip the first n rows
-            for i in range(rows_to_skip-1):
-                header = next(reader)
-
-            # Store the elements of the first column in a list
-            product_elements = [row[0] for row in reader if row[0].strip()]
-
             if header[0] == 'ELEMENT NAME':
+                rows_to_skip = 1
                 columns_to_skip = 2
                 name_column = 0
             elif header[1] == 'ELEMENT NAME':
+                rows_to_skip = 1
                 columns_to_skip = 3
                 name_column = 1
 
-            # Store the elements of the ELEMENT NAME column in a list
+            if header[1] == '':
+                rows_to_skip = 4
+                # Skip the first n rows
+                for i in range(rows_to_skip-1):
+                    header = next(reader)
+
+                if header[0] == 'ELEMENT NAME':
+                    columns_to_skip = 2
+                    name_column = 0
+                elif header[1] == 'ELEMENT NAME':
+                    columns_to_skip = 3
+                    name_column = 1
+            else:
+                rows_to_skip = 1
+                # Skip the first n rows
+                for i in range(rows_to_skip-1):
+                    header = next(reader)
+
+                if header[0] == 'ELEMENT NAME':
+                    columns_to_skip = 2
+                    name_column = 0
+                elif header[1] == 'ELEMENT NAME':
+                    columns_to_skip = 3
+                    name_column = 1
+            
+            # Calculate the dimension of the matrix
+            dimension = col_count - columns_to_skip
+            
+            # Store the elements of the first column in a list
             product_elements = [row[name_column] for row in reader if row[name_column].strip()]
-            #st.write(product_elements)
+
+            # st.write(f"rows_to_skip: {rows_to_skip}")
+            # st.write(f"columns_to_skip: {columns_to_skip}")
+            # st.write(f"name_column: {name_column}")
+            # st.write(f"header: {header}")
+            # st.write(f"col_count: {col_count}")
+            # st.write(f"row_count: {row_count}")
+            # st.write(f"dimension: {dimension}")
+            # st.write(f"product_elements: {product_elements}")
             
             # Go back to the start of the file
             file.seek(0)
@@ -100,15 +131,18 @@ if ('uploaded_files' in locals()) and (uploaded_files != []):
                 next(reader)
 
             if col_count - 2 == row_count - 4:
-                st.write('This is a simple matrix')
+                #st.write('This is a simple matrix')
                 matrix = np.zeros((dimension, dimension))
                 for i, row in enumerate(reader):
                     for j, element in enumerate(row[columns_to_skip:]):
                         if element.strip():
+                            #st.write(i, j, element)
                             matrix[i, j] = float(element)
-                st.write(matrix)
+                #st.write(matrix)
 
                 design_structure_matrix = [[1 if x != 0 else x for x in sublist] for sublist in matrix]
+                direct_likelihood_matrix = matrix
+                direct_impact_matrix = matrix
 
                 # Calculate all matrices
                 likelihood_matrix, risk_matrix, impact_matrix = calculate_all_matrices(
@@ -120,8 +154,7 @@ if ('uploaded_files' in locals()) and (uploaded_files != []):
 
             #elif col_count - 2 == int((row_count - 3) / 2):
             else:
-                st.write('This is a double matrix')
-                dimension = col_count - columns_to_skip
+                #st.write('This is a double matrix')
                 direct_likelihood_matrix = np.zeros((dimension, dimension))
                 direct_impact_matrix = np.zeros((dimension, dimension))
                 for i, row in enumerate(reader):
@@ -145,33 +178,65 @@ if ('uploaded_files' in locals()) and (uploaded_files != []):
                     direct_impact_matrix,
                     change_path_length
                 )
-                
-            #st.write('Likelihood matrix')
-            #st.write(np.array(likelihood_matrix))
+            
+            col1, col2 = st.columns(2)
 
-            #st.write('Impact matrix')
-            #st.write(np.array(impact_matrix))
+            with col1:
+                with st.expander('Show direct likelihood matrix'):
+                    st.write(np.array(direct_likelihood_matrix))
+                with st.expander('Show direct impact matrix'):
+                    st.write(np.array(direct_impact_matrix))
+                with st.expander('Show design structure matrix'):
+                    st.write(np.array(design_structure_matrix))
+                with st.expander('Show combined likelihood matrix'):
+                    st.write(np.array(likelihood_matrix))
+                with st.expander('Show combined impact matrix'):
+                    st.write(np.array(impact_matrix))
+                with st.expander('Show combined risk matrix'):
+                    st.write(np.array(risk_matrix))
+                # Save the list of lists to a CSV text
+                file = io.StringIO()
+                writer = csv.writer(file)
+                writer.writerows(risk_matrix)
+                result_files[uploaded_file.name] = file.getvalue()
 
-            st.write('Risk matrix')
-            st.write(np.array(risk_matrix))
 
-            # Save the list of lists to a CSV text
-            file = io.StringIO()
-            writer = csv.writer(file)
-            writer.writerows(risk_matrix)
-            result_files[uploaded_file.name] = file.getvalue()
-
-            # def callback():
-            #    st.balloons()
-            # Provide a download button
-            st.download_button(
-                label="Press to Download",
-                data=file.getvalue(),
-                file_name="risk_"+uploaded_file.name,
-                mime="text/csv",
-                #on_click=callback,
-                #key='callback'
-            )
+            with col2:
+                # def callback():
+                #    st.balloons()
+                # Provide a download button
+                st.download_button(
+                    label="Download Combined Risk Matrix",
+                    data=file.getvalue(),
+                    file_name="risk_"+uploaded_file.name,
+                    mime="text/csv",
+                    #on_click=callback,
+                    #key='callback'
+                )
+                fig = px.imshow(
+                    risk_matrix,
+                    labels=dict(x="", y="", color="Risk"),
+                    x=product_elements,
+                    y=product_elements,
+                    color_continuous_scale='dense',
+                    #title='Combined Risk Matrix',
+                    width=800,
+                    height=800,
+                    #text_auto='.2f',
+                    aspect='equal',
+                )
+                fig.update_layout(
+                    xaxis={"side": "top"},
+                    yaxis={'side': 'left'}  
+                )
+                st.plotly_chart(
+                    fig, 
+                    use_container_width=True,
+                    config={
+                        'displaylogo': False,
+                        'modeBarButtonsToRemove': ['sendDataToCloud', 'select2d', 'lasso2d', 'zoomIn2d', 'zoomOut2d', 'autoScale2d', 'resetScale2d', 'hoverClosestCartesian', 'hoverCompareCartesian', 'toggleSpikelines']
+                    }
+                )
 
             st.markdown('---')
 
@@ -180,8 +245,6 @@ if ('uploaded_files' in locals()) and (uploaded_files != []):
         # Create a zip file
         with zipfile.ZipFile(f"{timestr}results.zip", mode="w") as archive:
             for file in result_files:
-                print(file)
-                print(result_files[file])
                 archive.writestr("risk_"+file, data=result_files[file])
         # Provide a download button
         with open(f"{timestr}results.zip", "rb") as file:
